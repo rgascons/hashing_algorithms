af39c6646e4f304e1016a68541cd1be6ead1d0f8
pau oliver <pau.oliver@est.fib.upc.edu>
2016-01-05 02:52:42 +0100

    Binary search follows Set interface, function refactored to find(k), unsigned ints.

--- binary_search.cc
+++ binary_search.cc
@@ -1,24 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class BinarySearch {
-public:
+
+private:
+	
+	vector<unsigned int> v;
 
 	// Pre: The vector v is sorted in strictly increasing order. Moreover, we have 0 ≤ left ≤ v.size() and −1 ≤ right < v.size()
-	int binary_search(const vector<int>& v, int key, int left, int right) {
+	int binarySearch(unsigned int key, int left, int right) {
 		if (right < left) return -1;
 
 	  	int mid = (left+right)/2;
-	  	if (v[mid] > key) return binary_search(v, key, left, mid - 1);
-	  	if (v[mid] < key) return binary_search(v, key, mid + 1, right);
+	  	if (v[mid] > key) return binarySearch(key, left, mid - 1);
+	  	if (v[mid] < key) return binarySearch(key, mid + 1, right);
 	  	return mid;
+	}
+
+public:
+
+	void setDictionary(vector<unsigned int> v) {
+		sort(v.begin(), v.end());	// implement quick sort/radix sort?
+		this->v = v;
+	}
+
+	int find(unsigned int k) {
+		return binarySearch(k, 0, v.size()-1);
 	}
 	
 };
 
 int main() {
 	BinarySearch bs;
-	vector<int> v = {5,7,10,12,15,20};
-	cout << bs.binary_search(v, 5, 0, v.size()-1) << endl;
+	vector<unsigned int> v = {5,7,10,12,15,20};
+	bs.setDictionary(v);
+	cout << bs.find(5) << endl;
+	cout << bs.find(12) << endl;
+	cout << bs.find(20) << endl;
 }
