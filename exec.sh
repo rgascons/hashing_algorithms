3ed139a8f5dbff10263888520588d8e331a8c537
pau oliver <pau.oliver@est.fib.upc.edu>
2016-01-11 20:46:26 +0100

    new results, cuckoo failing at 9th exec

--- exec.sh
+++ exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {1..10}
 do
-	>&2 echo "execution $i: main dict/dict$i queries/query$i 3 $((10#$i * 200000)) $((10#$i * 10000000)) 0.6 $((10#$i * 200000))"
-	echo "execution $i: main dict/dict$i queries/query$i 3 $((10#$i * 200000)) $((10#$i * 10000000)) 0.6 $((10#$i * 200000))"
-	main "dict/dict$i" "queries/query$i" 3 "$((10#$i * 200000))" "$((10#$i * 10000000))" 0.6 "$((10#$i * 200000))"
+	>&2 echo "execution $i: main dicts/dict$i queries/query$i 3 $((10#$i * 200000)) $((10#$i * 10000000)) 0.6 $((10#$i * 200000))"
+	echo "execution $i: main dicts/dict$i queries/query$i 3 $((10#$i * 200000)) $((10#$i * 10000000)) 0.6 $((10#$i * 200000))"
+	main "dicts/dict$i" "queries/query$i" 3 "$((10#$i * 200000))" "$((10#$i * 10000000))" 0.6 "$((10#$i * 200000))"
 done
